77115fade35ab74468a2e5eaf713c24d4ca2491a
ProphetDiceBot <prophetdice@gmail.com>
2024-12-07 14:45:27 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pdice',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'undetected-chromedriver',
+        'asyncio',
+        'tqdm',
+        'pytesseract',
+        'spacy',
+        'pymupdf',
+        'google-generativeai',
+        'mistralai',
+        'imblearn',
+        'requests',
+        'fastapi',
+        'uvicorn',
+        'pyautgui',
+        'crewai',
+        'pywebio',
+        'pywebio-battery',
+
+    ],
+    author='Prophet Dice Bot',
+    author_email='support@prophetdice.com',
+    description='ProphetDice Functions',
+    url='https://prophetdice.com',
+)
